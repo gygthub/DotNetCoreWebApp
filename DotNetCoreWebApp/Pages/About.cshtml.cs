e59af8d5658e6b44ef70d9966697c3afd13353e6
gygthub <younessgmarr@gmail.com>
2021-08-24 13:04:58 +0100

    new folder added

---
+++ DotNetCoreWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCoreWebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Now Build and release process ready, Lets check in new changes to deploy the code into IIS web server";
+        }
+    }
+}
